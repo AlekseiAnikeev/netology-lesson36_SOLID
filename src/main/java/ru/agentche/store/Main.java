3dffcce24def62833b9cf57fefb9584670d650f6
AlekseiAnikeev <info.anikeev@gmail.com>
2022-10-28 16:31:05 +0300

    init commit

---
+++ src/main/java/ru/agentche/store/Main.java
@@ -0,0 +1,41 @@
+package ru.agentche.store;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+/**
+ * @author Aleksey Anikeev aka AgentChe
+ * Date of creation: 25.10.2022
+ */
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        HashMap<String, Integer> products = new HashMap<>();
+        products.put("Хлеб", 56);
+        products.put("Масло", 153);
+        products.put("Колбаса", 211);
+        products.put("Пирожок", 45);
+
+        System.out.println("В МАГАЗИНЕ В НАЛИЧИИ");
+        for (Map.Entry<String, Integer> productAndPrice : products.entrySet()) {
+            System.out.println(productAndPrice.getKey() + " за " + productAndPrice.getValue() + " руб./шт.");
+        }
+
+        System.out.println("Введите два слова: название товара и количество. Или end");
+        Scanner scanner = new Scanner(System.in);
+        Purchase purchase = new Purchase();
+        while (true) {
+            String line = scanner.nextLine();
+            if ("end".equals(line)) break;
+            String[] parts = line.split(" ");
+            String product = parts[0];
+            int count = Integer.parseInt(parts[1]);
+            purchase.addPurchase(product, count);
+        }
+        long sum = purchase.sum(products);
+        System.out.println("ИТОГО: " + sum);
+    }
+}
